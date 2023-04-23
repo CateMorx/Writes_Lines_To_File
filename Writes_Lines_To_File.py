eaf879ed32bbd83bf0ebbcd9d4a7b1a463dce9a7
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-04-23 21:31:16 +0800

    adds python file with psuedocode

---
+++ Writes_Lines_To_File.py
@@ -0,0 +1,19 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Assignment 3 
+#Write a method in python to write multiple line of text contents into a text file mylife.txt.
+
+#Imports necessary elements
+
+#Creates Method that allows user to write to "my_file.txt" text file
+#opens and writes to "my_file.txt" 
+#Prompts user for input
+#Writes the input in the file
+#Asks user if they want to input more lines
+#Allows user to input another line if Yes
+#Ends the program if No
+
+#Calls Method
+
+#Creates a Method to Display the "my_file.txt" file with GUI
+
+#Calls Method
