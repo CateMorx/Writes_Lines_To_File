13c4dcfd6a2b015bd4b1a5f21f29dabb6f759f5d
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-04-23 21:32:42 +0800

    imports elemets, creates method

--- Writes_Lines_To_File.py
+++ Writes_Lines_To_File.py
@@ -3,9 +3,13 @@
 #Write a method in python to write multiple line of text contents into a text file mylife.txt.
 
 #Imports necessary elements
+import PySimpleGUI as psg
+import tkinter as tk
 
 #Creates Method that allows user to write to "my_file.txt" text file
-#opens and writes to "my_file.txt" 
+def write_file():
+    #opens and writes to "my_file.txt"
+    with open("my_file.txt", "w") as file: 
 #Prompts user for input
 #Writes the input in the file
 #Asks user if they want to input more lines
